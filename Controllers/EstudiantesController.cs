e8e05fd696451c3f5effa244941ebc2a9e123976
Omargenao25 <omargenao78@gmail.com>
2026-06-13 11:50:35 -0400

    creacion de lista privada estatica donde se guardaran los estudiantes.

--- Controllers/EstudiantesController.cs
+++ Controllers/EstudiantesController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ActividadPractica2.Models;
 
 namespace ActividadPractica2.Controllers
 {
-    public class EstudiantesController : Controller
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class EstudiantesController : ControllerBase
     {
-        public IActionResult Index()
+
+        private static List<Estudiante> estudiantes = new List<Estudiante>()
         {
-            return View();
-        }
+
+            new Estudiante
+            {
+                Id = 1,
+                Nombre = "Ana",
+                Apellido = "Perez",
+                Correo = "ana@gmail.com",
+                Carrera = "Ingenieria de Sistemas",
+                Edad = 20,
+                Promedio = 85,
+                Activo = true
+            },
+
+
+            new Estudiante
+            {
+                Id = 2,
+                Nombre = "Luis",
+                Apellido = "Fortuna",
+                Correo = "luis@gmail.com",
+                Carrera = "Contabilidad",
+                Edad = 22,
+                Promedio = 60,
+                Activo = false
+            }
+
+        };
+
+
     }
 }
+
